919372cb696a5f7b416e09913cc908aa7756bb75
trollcat <76234965+CozzyBro2@users.noreply.github.com>
2021-07-25 23:39:33 -0400

    make

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
+const GUESS_SIZE: usize = 5;
+
 fn main() {
-    println!("Hello, world!");
+    let mut guess = String::with_capacity(GUESS_SIZE);
+
+    io::stdin().read_line(&mut guess);
 }
